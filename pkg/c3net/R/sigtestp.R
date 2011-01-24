ecb2ba0972de0b83d33132b58e5b0bb86a1a5660
gokmen (Gokmen Altay) <gokmenaltay@gmail.com>
2011-01-24 12:05:57 +0000

    update DESCRIPTION
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/c3net@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/c3net/R/sigtestp.R
+++ pkg/c3net/R/sigtestp.R
@@ -2,16 +2,15 @@
 #c3net: C3NET, <https://r-forge.r-project.org/projects/c3net/>
 #This R package allows inferring regulatory networks from expression data using C3NET.
 #The inferred network consists of only direct physical interactions.
-## Copyright (C) August 2010 Gokmen Altay <altayscience@gmail.com>
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-## as published by the Free Software Foundation; either version 3
-## of the License, or any later version.
+## Copyright (C) January 2011 Gokmen Altay <altayscience@gmail.com>
+## This program is a free software for only academic useage but not for commercial useage; you can redistribute it and/or
+## modify it under the terms of the GNU GENERAL PUBLIC LICENSE
+## either version 3 of the License, or any later version.
 ##
 ## This program is distributed WITHOUT ANY WARRANTY; 
-## You can get a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+## You can get a copy of the GNU GENERAL PUBLIC LICENSE
 ## from
-## http://www.r-project.org/Licenses/LGPL-3
+## http://www.gnu.org/licenses/gpl.html
 ## See the licence information for the dependent package from
 ## igraph package itself.
 
